3b4f665a513b77a0ed4ad8db4e96b9d25c120809
iamthes <s91630277@gmail.com>
2012-10-22 22:10:44 +0400

    Script favico, search module

--- Site/js/functions.js
+++ Site/js/functions.js
@@ -18,9 +18,32 @@ function insertHtml(file, to) {
 	request.open('GET', file, false); 
 	request.onreadystatechange = function() {
 		if (request.readyState == 4) {
-			to.innerHTML = request.responseText;
+			var params = {
+				request: request,
+				responseText: request.responseText
+			};
+			if (typeof to == 'function') to(params);
+			else to.innerHTML = request.responseText;
 		}
 	}
 	request.send();
 }
 
+function addFavIcons() {
+	var rootCollection = document.getElementsByTagName('section');
+	for (var n = 0; n < rootCollection.length; n++) {
+		var root = rootCollection[n];
+		var anchors = root.getElementsByTagName('a');
+		for (var i = 0, length = anchors.length; i < length; i++) {
+			var anchor = anchors[i];
+			if (anchor.parentNode.nodeName != "P") continue;
+			var href = anchor.getAttribute('href');
+			if (href.indexOf('//') != -1) {
+				var iconUrl = 'http://g.etfv.co/' + encodeURIComponent(href);
+				anchor.setAttribute('style', 'padding-left: 20px; background: transparent url('+iconUrl+') no-repeat center left');
+			}
+		}
+	}
+}
+
+//attr('style', 'padding-left: 20px; background: transparent url('+url+') no-repeat center left');
--- Site/js/global.js
+++ Site/js/global.js
@@ -3,11 +3,36 @@ bindReady(function(){
 	var siteMenuWrapper = document.getElementById('SiteMenuWrapper');
 	insertHtml('menu.html', siteMenuWrapper);
 
-	// 1. Replace of #CurrentYear
+	insertHtml('search-module.html', function(params) {
+		var responseText = params.responseText;
+		var panel = document.getElementById('Panel');
+		panel.innerHTML += responseText;
+		var form = document.getElementById('SideSearchForm');
+		form.addEventListener('submit', function(e) {
+			var search = form.Search.value;
+			var host = document.location.host;
+			if (host != "") {
+				var q = encodeURIComponent(search + ' site:' + host);
+				var url  = "https://www.google.ru/?q=" + q + '#q=' + q;
+				window.open(url);
+			}
+			e.preventDefault();
+			return false;
+		});
+	});	
+
+	// Adding favicons.
+	addFavIcons();
+
+	// Replace of #CurrentYear
 	var currentYear = document.getElementById('CurrentYear');
 	currentYear.innerHTML = (new Date()).getFullYear();
 
-	// 2. Pinned footer
+	if (typeof fixie != 'undefined') {
+		fixie.init();
+	}
+
+	// Last. Pinned footer
 	var frame = document.getElementById('Frame');
 	PinnedFooter({frame: frame});
 
